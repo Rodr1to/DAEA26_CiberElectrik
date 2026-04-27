6f72f7f0cac980150319ab63096640817aa084ed
rodr1to <rovalverdea@gmail.com>
2026-04-26 21:06:57 -0500

    DAEA 2026-10

---
+++ src/main/java/pe/com/ciberelectrik/CiberElectrikApplication.java
@@ -0,0 +1,13 @@
+package pe.com.ciberelectrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiberElectrikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiberElectrikApplication.class, args);
+    }
+
+}
